73b69f64afe0e43855f6c917dff2d1e6ddf7af95
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-12-12 17:08:16 -0800

    done turkey

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -82,7 +82,7 @@ function App() {
         featherCount={featherCount}
         featherColors={featherColors}
       />
-      <Message size={size} />
+      <Message size={size} featherCount={featherCount} />
     </>
   );
 }
--- hooks/src/components/Message.js
+++ hooks/src/components/Message.js
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
   const [sizeClass, setSizeClass] = useState("")
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    if (featherCount <= 0) {
+      setMessage('Oh my! Your bird is naked!')
+    } else if (featherCount >= 10) {
+      setMessage('Full turkey! Time to eat him.')
+    } else {
+      setMessage('Coming along...')
+    }
+  }, [featherCount])
   useEffect(() => {
     // console.log('PictureDisplay size', size)
     let className = ""
@@ -25,11 +35,13 @@ function Message({ size }) {
     setSizeClass(className);
   }, [size])
 
-  return (
+    return (
     <div className={`message ${sizeClass}`}>
-      (Oh my! Your bird is naked!)
-    </div>
-  );
+      {message}
+    </div> 
+    )
+
+
 };
 
 export default Message;
